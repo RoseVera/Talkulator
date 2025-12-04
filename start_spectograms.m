e75648105feac2d8f2ea52b9607326d6faf9a330
RoseVera <gulvera2003@gmail.com>
2025-12-04 18:20:53 +0100

    last updates

---
+++ start_spectograms.m
@@ -0,0 +1,5 @@
+% Define required directories
+audioDir = 'vera_canan_kaggle_data/';
+spectrogramDir = 'vera_canan_kaggle_spec/';
+spectrogramDimensions = [64, 64];
+generateSpectrograms(audioDir, spectrogramDir, spectrogramDimensions);
